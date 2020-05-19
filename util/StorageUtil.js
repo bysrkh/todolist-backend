41236eecb6cf630268f3264a49b3606212b80689
bysrkh <bysrkh@gmail.com>
2020-05-19 22:39:49 +0700

    changes in StorageUtil.js

--- util/StorageUtil.js
+++ util/StorageUtil.js
@@ -13,48 +13,39 @@ class StorageUtil {
     constructor({destination, filename}) {
         this.getDestination = destination
         this.getFilename = filename
+        this.s3 = new AWS.S3({
+            endpoint: process.env.S3_HOST,
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_KEY
+        })
     }
 
-    _handleFile = (req, file, cb) => (
-        this.getDestination(req, file, (err, bucketName) => {
-            if (err) return cb(err)
+    _handleFile = (req, file, callback) => (
+        this.getDestination(req, file, (err, fileBucket) => {
+            if (err) return callback(err)
 
             this.getFilename(req, file, (err, fileName) => {
-                const s3 = new AWS.S3({
-                    endpoint: process.env.S3_HOST,
-                    accessKeyId: process.env.S3_ACCESS_KEY,
-                    secretAccessKey: process.env.S3_SECRET_KEY
-                })
 
                 function outStream(s3) {
                     const pass = new stream.PassThrough();
                     s3.upload({
-                        Bucket: bucketName,
-                        Key: fileName,
-                        Body: pass
+                        Bucket: fileBucket, Key: fileName, Body: pass
                     }, {
-                        partSize: 5242880,
-                        queueSize: 1
-                    }, function (err, data) {
-                        if (err) cb(err)
-                        if (data) cb(null, {
-                            path: path,
-                            fileName: fileName,
-                            size: file.size
-                        })
+                        partSize: 5242880, queueSize: 1
+                    }, (err, data) => {
+                        if (err) return callback(err)
+                        if (data) callback(null, {path: data.Location, fileName: data.Key, size: file.size})
                     })
 
                     return pass
                 }
 
-                file.stream.pipe(outStream(s3))
+                file.stream.pipe(outStream(this.s3))
             })
         })
     )
 
-    _removeFile = (req, file, cb) => {
-        fs.unlink(file.path, cb)
-    }
+    _removeFile = (req, file, callback) => { } //TODO: leave it empty for a while
 }
 
 module.exports = opts => new StorageUtil(opts)
